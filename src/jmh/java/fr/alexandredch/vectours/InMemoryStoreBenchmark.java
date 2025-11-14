12ca187d3df856f1b93280f27720528505cfc964
Alexandre Duchesne <contact@limpsword.fr>
2025-11-14 19:40:21 +0100

    fix benchmark to wait for insertions

--- src/jmh/java/fr/alexandredch/vectours/InMemoryStoreBenchmark.java
+++ src/jmh/java/fr/alexandredch/vectours/InMemoryStoreBenchmark.java
@@ -3,6 +3,9 @@ package fr.alexandredch.vectours;
 import fr.alexandredch.vectours.data.SearchParameters;
 import fr.alexandredch.vectours.data.Vector;
 import fr.alexandredch.vectours.store.base.InMemoryStore;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
@@ -19,9 +22,12 @@ public class InMemoryStoreBenchmark {
         public void setUp() {
             store = new InMemoryStore();
             store.initFromDisk();
+            List<CompletableFuture<Void>> futures = new ArrayList<>();
             for (int i = 0; i < 1_000_000; i++) {
-                store.insert(new Vector("id" + i, new double[] {(double) i, (double) i + 1, (double) i + 2}, null));
+                futures.add(store.insert(
+                        new Vector("id" + i, new double[] {(double) i, (double) i + 1, (double) i + 2}, null)));
             }
+            CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
         }
 
         @TearDown(Level.Iteration)
@@ -43,9 +49,12 @@ public class InMemoryStoreBenchmark {
         public void setUp() {
             store = new InMemoryStore();
             store.initFromDisk();
+            List<CompletableFuture<Void>> futures = new ArrayList<>();
             for (int i = 0; i < 100_000; i++) {
-                store.insert(new Vector("id" + i, new double[] {(double) i, (double) i + 1, (double) i + 2}, null));
+                futures.add(store.insert(
+                        new Vector("id" + i, new double[] {(double) i, (double) i + 1, (double) i + 2}, null)));
             }
+            CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
             store.saveAll();
             store.shutdown();
             store = null;
